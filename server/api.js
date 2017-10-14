8293f2fe0e07856a4f58e4c788e60c01c207ce17
Nabil Wardeh <nabilwardeh@gmail.com>
2017-10-14 22:50:49 +0100

    completed all and added necessay checks to roots and foreign key constraints to database

--- server/api.js
+++ server/api.js
@@ -3,7 +3,27 @@ const router = express.Router();
 const filename = './database/database.sqlite';
 const sqlite3    = require( 'sqlite3' ).verbose();
 
+// Utility functions
+function isValidDate(date) {
+  return /^\d{4}-\d{2}-\d{2}$/.test(date);
+}
 
+function formatJSDate(date) {
+  let dateObj = new Date(date);
+  return `${dateObj.getFullYear()}-${('0' + (dateObj.getMonth() + 1)).slice(-2)}-${('0' + dateObj.getDate()).slice(-2)}`;
+}
+
+function getJSDate(date) {
+  return Date.parse(date);
+}
+
+function isValidateInvoice(invoice) {
+  if (Number.isNaN(invoice.total)) return false;
+  if (Number.isNaN(invoice.surcharges)) return false;
+  if (!isValidDate(invoice.invoiceDate)) return false;
+  if (invoice.paid.toLowerCase() !== 'true' &&  invoice.paid.toLowerCase() !== 'false') return false;
+  return true;
+}
 
 // open the database
 let db = new sqlite3.Database(filename);
@@ -17,8 +37,7 @@ router.get('/customers', function(req, res) {
     db.all(sql, [],(err, rows ) => {
       if (err) {
         console.log(err);
-        res.status(500).json({error: 'Failed to retrive customer data'});
-        return;
+        return res.status(500).json({error: `Failed to retrive customer data: ${err.message}`});
       }
       res.status(200).json({
         customers: rows
@@ -34,11 +53,10 @@ router.post('/customers', function(req, res) {
   db.serialize(function() {
     let sql = `INSERT INTO customers (title, firstname, surname, email) VALUES (?, ?, ?, ?)`;
 
-    db.run(sql, [customer.title, customer.firstname, customer.surname, customer.email], (err, rows ) => {
+    db.run(sql, [customer.title, customer.firstname, customer.surname, customer.email], function(err, rows ) {
       if (err) {
         console.log(err);
-        res.status(500).json({error: 'Failed to save customer data'});
-        return;
+        return res.status(500).json({error: `Failed to save customer data: ${err.message}`});
       }
       res.status(200).json({
         customers: rows
@@ -52,11 +70,10 @@ router.get('/room-types', function(req, res) {
   db.serialize(function() {
     var sql = `SELECT id, type_name AS name, original_price, current_price FROM room_types ORDER BY current_price ASC`;
 
-    db.all(sql, [], (err, rows ) => {
+    db.all(sql, [], function(err, rows ) {
       if (err) {
         console.log(err);
-        res.status(500).json({error: 'Failed to retrive rooms data'});
-        return;
+        return res.status(500).json({error: `Failed to retrive rooms data: ${err.message}`});
       }
       res.status(200).json({
         roomtypes: rows
@@ -66,213 +83,208 @@ router.get('/room-types', function(req, res) {
 });
 
 router.put('/discount', function(req, res) {
+  let discount = parseFloat( req.body.discount );
+  let roomId = parseFloat(req.query.id);
+
+  if (Number.isNaN(discount)) 
+    return res.status(400).json({error: 'Invalid input, discount accepts numbers'});
+
+  if (Number.isNaN(roomId)) 
+    return res.status(400).json({error: 'Invalid input, id accepts numbers'});
+
   let reductionFactor = 1-(req.body.discount / 100);
-  let fetchPriceSql = `SELECT original_price FROM room_types WHERE id = ${req.query.id}`;
-  let postPriceSql = `UPDATE room_types SET current_price = round((${fetchPriceSql}) * ${reductionFactor}) WHERE id = ${req.query.id}`;
+  let fetchPriceSql = `SELECT original_price FROM room_types WHERE id = ${roomId}`;
+  let postPriceSql = `UPDATE room_types SET current_price = round((${fetchPriceSql}) * ${reductionFactor}) WHERE id = ${roomId}`;
 
   db.serialize(function() {
-    db.run(postPriceSql, [], (err, data ) => {
+    db.run(postPriceSql, [], function(err, data ) {
       if (err) {
-      console.log(err);
-      res.status(500).json({error: 'Failed to update room data'});
-      return;
-    }
+        console.log(err);
+        return res.status(500).json({error: `Failed to update room data: ${err.message}`}); 
+      }
+      if (this.changes === 0) 
+        return res.status(400).json({error: 'Invalid room id, no records have been updated'});
       res.status(200).json({
-        id: req.query.id,
-        discount: req.body.discount
+        id: roomId,
+        discount: discount
       });  
     });
   });
 
-  /*
-  db.serialize(function() {
-    let fetchSql = `SELECT original_price FROM room_types WHERE id = ${req.query.id}`;
-    db.all(fetchSql, [], (err, rows ) => {
-      if (err) {
-        console.log(err);
-        res.status(500).json({error: 'Failed to retrive rooms data'});
-        return;
-      }
-      originalPrice = rows[0].original_price;
-
-      let postSql = `UPDATE room_types SET current_price = ${Math.round((1-(req.body.discount /100)) * original_price)} WHERE id = ${req.query.id}`;
-
-      db.run(postSql, [], (err, data ) => {
-        if (err) {
-        console.log(err);
-        res.status(500).json({error: 'Failed to update room data'});
-        return;
-      }
-        res.status(200).json({
-          id: req.query.id,
-          discount: req.body.discount
-        });  
-      });
-    });
-  }); */
 });
 
 router.post('/reservations', function(req, res) {
-  let nightPrice, fullPrice;
-  if (!(/^\d{4}-\d{2}-\d{2}$/.test(req.body.reservation.checkInDate)) ||
-      !(/^\d{4}-\d{2}-\d{2}$/.test(req.body.reservation.checkOutDate)) )
-    return res.status(500).jason({error: 'invalid dates'});
-  let checkInDate = new Date(Date.parse(req.body.reservation.checkInDate) + 14 * 3600000);
-  let checkOutDate = new Date(Date.parse(req.body.reservation.checkOutDate) + 12 * 3600000);
-  if (checkOutDate <= checkInDate || checkInDate < (new Date() + 10 * 3600000 - 1) ) return res.status(500).json({reservation: {error: 'Invalid dates'}});
-  let checkInDateString = `${checkInDate.getFullYear()}-${('0' + (checkInDate.getMonth() + 1)).slice(-2)}-${('0' + checkInDate.getDate()).slice(-2)} ${('0' + checkInDate.getHours()).slice(-2)}:${('0' + checkInDate.getMinutes()).slice(-2)}:${('0' + checkInDate.getSeconds()).slice(-2)}`;
-  let checkOutDateString = `${checkOutDate.getFullYear()}-${('0' + (checkOutDate.getMonth() + 1)).slice(-2)}-${('0' + checkOutDate.getDate()).slice(-2)} ${('0' + checkOutDate.getHours()).slice(-2)}:${('0' + checkOutDate.getMinutes()).slice(-2)}:${('0' + checkOutDate.getSeconds()).slice(-2)}`;
-  let roomId = req.body.reservation.roomId;
+  let nightPrice;
   let customerId = req.body.reservation.customerId;
+  let roomId = req.body.reservation.roomId;
+  let checkInDateString = req.body.reservation.checkInDate;
+  let checkOutDateString =req.body.reservation.checkOutDate;
+
+  // this validation should be moved to a utility function
+  if (!isValidDate(checkInDateString) || !isValidDate(checkOutDateString )) 
+    return res.status(400).json({error: 'invalid date format. Accepts: YYYY-MM-DD'});
+
+  if(!/^\d{1,}$/.test(customerId))
+    return res.status(400).json({error: 'invalid customerId, accepts numbers'});
+
+  if(!/^\d{1,}$/.test(roomId))
+    return res.status(400).json({error: 'invalid roomId, accepts numbers'});
+
+  let checkInDate = getJSDate(checkInDateString);
+  let checkOutDate = getJSDate(checkOutDateString);
+  let currentDate = new Date().setHours(0, 0, 0, 0);
+
+  if (checkInDate < currentDate || checkInDate > checkOutDate) 
+    return res.status(400).json({error: 'invalid bookign dates'});
+
+  console.log(checkInDate + ' ===== ' + checkOutDate);
+
   db.serialize(function() {
+
     let fetchPriceSql = `SELECT room_types.current_price AS price FROM rooms INNER JOIN room_types ON room_types.id = rooms.room_type_id  WHERE rooms.id = ${roomId}`;
-    
-    db.all(fetchPriceSql, [], (err, rows ) => {
-      if (err) return res.status(500).json({reservation: {error: 'Fialed to retrive night cost'}});
-      nightPrice = rows[0].price;
+    db.all(fetchPriceSql, [], function(err, fetchPriceRows ) {
+      if (err) {
+        console.log(err)
+        return res.status(500).json({reservation: {error: `Fialed to retrive night cost ${err.message}`}});
+      }
+      if (fetchPriceRows.length < 1) 
+        return res.status(400).json({error: 'roonId does not exist'});
 
-      let checkExistingReservationsSql = `SELECT room_id, check_in_date, check_out_date FROM reservations WHERE room_id = ${roomId}`;
+      nightPrice = fetchPriceRows[0].price;
 
-      db.all(checkExistingReservationsSql, [], (err, rows) => {
-        if (err) return res.status(500).json({reservation: {error: 'Failed to retrive existing booking data'}});
-        let bookingConflict = rows.filter(row => {
-          let existingCheckInDate = Date.parse(row.check_in_date);
-          let existingCheckOutDate = Date.parse(row.check_out_date);
-          return ((checkInDate >= existingCheckInDate && checkInDate <= existingCheckOutDate) ||
-           (checkOutDate >= existingCheckInDate && checkOutDate <= existingCheckOutDate) ||
-           (checkInDate <= existingCheckInDate && checkOutDate >= existingCheckOutDate))
-        }).length;
-        if (bookingConflict > 0) return res.status(500).json({reservation: {error: 'room booked for all or some of these dates'}});
+      let checkExistingReservationsSql = `SELECT room_id, check_in_date, check_out_date FROM reservations WHERE room_id = ${roomId} AND (${checkInDate} BETWEEN check_in_date AND check_out_date OR ${checkOutDate} BETWEEN check_in_date AND check_out_date OR check_in_date BETWEEN ${checkInDate} AND ${checkOutDate})`;
+
+      console.log(checkExistingReservationsSql);
+
+      db.all(checkExistingReservationsSql, [], function(err, fetchExistingBookingRows) {
+        if (err) {
+          console.log(err)
+          return res.status(500).json({reservation: {error: `Failed to retrive existing booking data: ${err.message}`}});
+        }
+        if (fetchExistingBookingRows.length > 0) 
+          return res.status(400).json({reservation: {error: 'room booked for all or some of these dates'}});
 
         let postReservationSql = `INSERT INTO reservations (customer_id, room_id, check_in_date, check_out_date, room_price) VALUES (?, ?, ?, ?, ?)`;
 
         fullPrice = Math.ceil((checkOutDate - checkInDate) / (24 * 3600000)) * nightPrice;
 
-         db.run(postReservationSql, [customerId, roomId, checkInDateString, checkOutDateString, fullPrice], (err, rows) => {
+        db.run(postReservationSql, [customerId, roomId, checkInDate, checkOutDate, fullPrice], function(err, postedRows) {
           if (err) {
             console.log(err);
-            return res.status(500).json({reservation: {error: 'Failed to set reservsation'}});
+            return res.status(500).json({reservation: {error: `Failed to set reservsation: ${err.message}`}});
           }
-
+          if (this.changes === 0) 
+            return res.status(500).json({reservation: {error: 'Data not posted, no records have been added'}});
           res.status(200).json({
-            reservation: rows
+            reservation: postedRows
           });
-         });
+        });
       });
-
-
-
-      // let postSql = `UPDATE room_types SET current_price = ${Math.round((1-(req.body.discount /100)) * original_price)} WHERE id = ${req.query.id}`;
-
-      /*db.run(postSql, [], (err, data ) => {
-        if (err) console.log(err);
-        res.status(200).json({
-          id: req.query.id,
-          discount: req.body.discount
-        });  
-      });*/
     });
-  }); 
+  });
+
 });
 
 router.get('/reservations', function(req, res) {
-  let condition = (req.query.id) ? `customer_id = ${req.query.id}` : `firstname = \'${req.query.name}\'`;
+  let condition = (req.query.id) ? `customer_id = ${req.query.id}` : `firstname LIKE \'%${req.query.name}%\'`;
+
   let reservationQuerySql = `SELECT title, firstname, surname, email, room_id AS roomId, check_in_date AS checkInDate, check_out_date AS checkOutDate FROM reservations INNER JOIN customers ON customers.id = reservations.customer_id WHERE ${condition}`;
 
   db.serialize(function() {
     db.all(reservationQuerySql, [], (err, rows) => {
       if (err) {
         console.log(err);
-        res.status(500).json({error: 'Failed to retrive reservation data'});
-        return;
+        return res.status(500).json({error: 'Failed to retrive reservation data'});
       }
-      res.status(200).json({ reservations: rows});
+      let rowsReadable = rows.map(row => {
+        row.checkInDate = formatJSDate(row.checkInDate);
+        row.checkOutDate = formatJSDate(row.checkOutDate);
+        return row;
+      });
+      res.status(200).json({ reservations: rowsReadable});
     });
   });
 });
 
 router.get('/reservations/date-from/:dateFrom', function(req, res) {
-  if (!(/^\d{4}-\d{2}-\d{2}$/.test(req.params.dateFrom))) return res.status(500).jason({error: 'invalid date'});
-  let checkInDate = new Date(req.params.dateFrom);
-  let endDate = new Date(Date.parse(req.params.dateFrom) + 30 * 24 * 3600000);
-  let checkInDateString = `${checkInDate.getFullYear()}-${('0' + (checkInDate.getMonth() + 1)).slice(-2)}-${('0' + checkInDate.getDate()).slice(-2)} ${('0' + checkInDate.getHours()).slice(-2)}:${('0' + checkInDate.getMinutes()).slice(-2)}:${('0' + checkInDate.getSeconds()).slice(-2)}`;
-  let endDateString = `${endDate.getFullYear()}-${('0' + (endDate.getMonth() + 1)).slice(-2)}-${('0' + endDate.getDate()).slice(-2)} ${('0' + endDate.getHours()).slice(-2)}:${('0' + endDate.getMinutes()).slice(-2)}:${('0' + endDate.getSeconds()).slice(-2)}`;
-  let condition = `check_in_date BETWEEN \'${checkInDateString}\' AND \'${endDateString}\'`;
+  let dateFrom = req.params.dateFrom;
+  if (!isValidDate(dateFrom)) 
+    return res.status(400).json({error: 'invalid date format: accepts YYYY-MM-DD'});
+
+  let checkInDate = Date.parse(dateFrom);
+  let endDate = checkInDate + 30 * 24 * 3600000;
+
+  let condition = `check_in_date BETWEEN \'${checkInDate}\' AND \'${endDate}\'`;
+
   let reservationsQuerySql = `SELECT title, firstname, surname, email, room_id AS roomId, check_in_date AS checkInDate, check_out_date AS checkOutDate FROM reservations INNER JOIN customers ON customers.id = reservations.customer_id WHERE ${condition} ORDER BY check_in_date ASC`;
 
-  console.log(reservationsQuerySql);
-
   db.serialize(function() {
-    db.all(reservationsQuerySql, [], (err, rows) => {
+    db.all(reservationsQuerySql, [], function(err, rows) {
       if (err) {
         console.log(err);
-        res.status(500).json({ error: 'Failed to retrive booking data'});
-        return;
+        return res.status(500).json({ error: `Failed to retrive booking data: ${err.message}`});
       }
-      let rowsHumanReadable = rows.map(row => {
-        let checkInDate = new Date(row.checkInDate);
-        let checkOutDate = new Date(row.checkOutDate);
-        return {
-          title: row.title,
-          firstname: row.firstname,
-          surname: row.surname,
-          email: row.email,
-          roomId: row.roomId,
-          checkInDate: `${checkInDate.getFullYear()}-${checkInDate.getMonth() + 1}-${checkInDate.getDate()}`,
-          checkOutDate: `${checkOutDate.getFullYear()}-${checkOutDate.getMonth() + 1}-${checkOutDate.getDate()}`
-        }; 
+      let rowsReadable = rows.map(row => {
+        row.checkInDate = formatJSDate(row.checkInDate);
+        row.checkOutDate = formatJSDate(row.checkOutDate);
+        return row;
       });
       res.status(200).json({
         reservations: rows
       });
     });
   });
-  // TODO read req.params.dateFrom to look up reservations and return
-/*  res.status(200).json({
-    reservations:[{
-      title: 'Mr',
-      firstname: 'Laurie',
-      surname: 'Ainley',
-      email: 'laurie@ainley.com',
-      roomId: 1,
-      checkInDate: '2017-10-10',
-      checkOutDate: '2017-10-17'
-    }, {
-      title: 'Miss',
-      firstname: 'Someone',
-      surname: 'Else',
-      email: 'someone@else.com',
-      roomId: 2,
-      checkInDate: '2017-11-12',
-      checkOutDate: '2017-11-15'
-    }]
-  });*/
+
 });
 
 router.put('/invoice', function(req, res) {
   let reservationId = req.query.reservationId;
   let invoice = req.body.invoice;
 
+  if (Number.isNaN(parseFloat(reservationId))) 
+    return res.status(400).json({error: 'Invalid reservationId: accepts numbers'});
+
+  if (!isValidateInvoice(invoice)) 
+    return res.status(400).json({error: 'Invalid invlice data: accepts numbers except for paid boolean'});
+
+  invoice.invoiceDate = getJSDate(invoice.invoiceDate);
+  invoice.paid = (invoice.paid.toLowerCase() === 'true');
+
   db.serialize(function() {
     postInvoiceSql = `INSERT INTO invoices (reservation_id, total, surcharges, invoice_date_time, paid) VALUES (?, ?, ?, ?, ?)`;
-    db.run(postInvoiceSql, [reservationId, invoice.total, invoice.surcharges, invoice.invoiceDate, invoice.paid], (err, rows) => {
+    db.run(postInvoiceSql, [reservationId, invoice.total, invoice.surcharges, invoice.invoiceDate, invoice.paid], function(err, rows) {
       if (err) {
         console.log(err);
-        res.status(500).json({ error: 'Failed to save invoice details'});
-        return
+        return res.status(500).json({ error: `Failed to save invoice details ${err.message}`});
       }
+      if (this.changes === 0) 
+        return res.status(500).json({invoice: {error: 'Data not posted, no records have been added'}});
       res.status(200).json({
         reservationId: req.query.reservationId,
         invoice: req.body.invoice
       });
     });
   });
-  // TODO read req.query.reservationId and req.body.invoice and insert into DB
 });
 
 router.post('/reviews', function(req, res) {
-  // TODO read req.body.review
-  res.status(200).json(req.body);
+  let review = req.body.review;
+  review.reviewDate = getJSDate(review.reviewDate);
+
+  let postReviewSql = `INSERT INTO reviews (customer_id, room_type_id, rating, comment, review_date) VALUES (?, ?, ?, ?, ?)`;
+
+  db.serialize(function() {
+    db.run(postReviewSql, [review.customerId, review.roomTypeId, review.rating, review.comment, review.reviewDate], function(err, rows) {
+      if (err) {
+        console.log(err);
+        return res.status(500).json({ error: `Failed to save reviw details ${err.message}`});
+      }
+      if (this.changes === 0) 
+        return res.status(500).json({invoice: {error: 'Data not posted, no records have been added'}});
+      res.status(200).json(req.body);
+    });
+  });
+
 });
 
 module.exports = router;
